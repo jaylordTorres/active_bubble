24461d2080422432e6f5d071dddf48650d47040c
Jay Lord P. Torres <jaylord@umpisa.co>
2020-12-08 20:52:45 +0800

    fix model

--- lib/class/Idea_model.dart
+++ lib/class/Idea_model.dart
@@ -1,11 +1,20 @@
-class IdeaModel {
+import 'package:active_bubble/class/user_model.dart';
+
+import 'base_model.dart';
+
+class IdeaModel implements BaseModel {
+  IdeaModel.dummy(
+      {this.content, this.fullName, this.userThumb, this.user, this.id});
+  IdeaModel.fromJson(data)
+      : id = data['id'],
+        content = data["content"],
+        userThumb = data["user_thumb"],
+        user = User.fromJson(data["user"]),
+        fullName = data["full_name"];
+
   final String content;
   final String fullName;
   final String userThumb;
+  final User user;
   final int id;
-  IdeaModel.fromJson(item)
-      : id = item['id'],
-        content = item["content"],
-        userThumb = item["user_thumb"],
-        fullName = item["full_name"];
 }

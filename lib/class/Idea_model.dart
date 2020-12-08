b290f4d541daa4d47909653205c6bbfb0e981a46
Jay Lord P. Torres <jaylord@umpisa.co>
2020-12-08 22:12:23 +0800

    remove full name on idea

--- lib/class/Idea_model.dart
+++ lib/class/Idea_model.dart
@@ -3,17 +3,14 @@ import 'package:active_bubble/class/user_model.dart';
 import 'base_model.dart';
 
 class IdeaModel implements BaseModel {
-  IdeaModel.dummy(
-      {this.content, this.fullName, this.userThumb, this.user, this.id});
+  IdeaModel.dummy({this.content, this.userThumb, this.user, this.id});
   IdeaModel.fromJson(data)
       : id = data['id'],
         content = data["content"],
         userThumb = data["user_thumb"],
-        user = User.fromJson(data["user"]),
-        fullName = data["full_name"];
+        user = User.fromJson(data["user"]);
 
   final String content;
-  final String fullName;
   final String userThumb;
   final User user;
   final int id;

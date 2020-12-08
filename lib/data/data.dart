2da423f5e14bd1d672003a2de70d709ee255fc55
Jay Lord P. Torres <jaylord@umpisa.co>
2020-12-08 20:22:10 +0800

    fake data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,1 +1,12 @@
-import '../class/class.dart';
+import 'dart:math';
+
+import 'package:faker/faker.dart';
+
+dynamic createIdea(int id) => ({
+      "id": id,
+      "full_name": faker.internet.userName(),
+      "content": faker.lorem.sentences(Random().nextInt(5) + 1).join("\n"),
+      "user_thumb": faker.image.image(width: 50, height: 50)
+    });
+
+final ideas = List.generate(20, createIdea);

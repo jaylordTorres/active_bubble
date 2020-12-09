8c7a754e2223e99a655de4d8c1bb8d9cb4c933f6
Jay Lord P. Torres <isens.jaylord@gmail.com>
2020-12-09 22:58:21 +0800

    fix guest init ui

--- lib/page/guest_init.dart
+++ lib/page/guest_init.dart
@@ -27,30 +27,37 @@ class _GuestInitPageState extends State<GuestInitPage> {
   Widget build(BuildContext context) {
     return UiPage(
         child: Scaffold(
-            body: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-            flex: 2,
-            child: Center(
-                child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('AB', style: Theme.of(context).textTheme.headline1),
-                Text(
-                  'Active Bubble',
-                  style: Theme.of(context).textTheme.headline3,
-                )
-              ],
-            ))),
-        Expanded(
-          flex: 1,
-          child: Container(),
-        ),
-        ElevatedButton(
-            onPressed: loading ? null : _loginAsGuest,
-            child: Text('Login as Guest'))
-      ],
+            body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              flex: 2,
+              child: Center(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('AB',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline1
+                          .copyWith(fontWeight: FontWeight.w300)),
+                  Text(
+                    'Active Bubble',
+                    style: Theme.of(context).textTheme.headline4,
+                  )
+                ],
+              ))),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          ElevatedButton(
+              onPressed: loading ? null : _loginAsGuest,
+              child: Text('Login as Guest'))
+        ],
+      ),
     )));
   }
 }

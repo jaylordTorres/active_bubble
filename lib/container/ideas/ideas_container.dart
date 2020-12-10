22041b6ffaf3c501b6de47ff5ebfe67204978c4c
Jay Lord P. Torres <isens.jaylord@gmail.com>
2020-12-10 22:44:06 +0800

    const

--- lib/container/ideas/ideas_container.dart
+++ lib/container/ideas/ideas_container.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../constant/main.dart';
 import '../../widget/idea_card.dart';
 import '../../class/idea_model.dart';
 
-class IdeasContainer extends StatelessWidget {
+class IdeasContainer extends StatefulWidget {
   const IdeasContainer({Key key, this.data}) : super(key: key);
   final List<IdeaModel> data;
 
   @override
+  _IdeasContainerState createState() => _IdeasContainerState();
+}
+
+class _IdeasContainerState extends State<IdeasContainer> {
+  Query query = FirebaseFirestore.instance.collection(FireCollections.article);
+  @override
   Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: query.snapshots(),
+        builder: (context, stream) {
+          if (stream.connectionState == ConnectionState.waiting) {
+            return LinearProgressIndicator();
+          }
+          if (stream.hasError) {
+            return Center(
+              child: Text(
+                stream.error.toString(),
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+                    .copyWith(color: Colors.red),
+              ),
+            );
+          }
+          QuerySnapshot querySnapshot = stream.data;
+          return body(context, querySnapshot);
+        });
+  }
+
+  Widget body(BuildContext context, QuerySnapshot data) {
     return CustomScrollView(
       slivers: [
         SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-            return IdeaCard(
-              key: Key(data[index].id.toString()),
-              idea: data[index],
-            );
-          }, childCount: data.length),
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return IdeaCard(
+                key: Key(data.docs[index].id),
+                idea: data.docs[index],
+              );
+            },
+            childCount: data.size,
+          ),
         )
       ],
     );

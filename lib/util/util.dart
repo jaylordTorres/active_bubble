b1ecd815d92428abe9070fc5aab4fa57fd039fdb
Jay Lord P. Torres <isens.jaylord@gmail.com>
2020-12-14 21:34:20 +0800

    remove uitl

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -2,4 +2,4 @@ export 'package:basic_utils/basic_utils.dart';
 export 'package:toast/toast.dart';
 export 'package:cached_network_image/cached_network_image.dart';
 
-export 'helper.dart';
+// export 'helper.dart';
